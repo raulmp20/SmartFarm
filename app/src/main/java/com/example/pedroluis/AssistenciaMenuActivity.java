ef42b74c0a8d077d9090a43968a99df5d9c4ef94
raulmp20 <raulmp02@hotmail.com>
2023-05-22 16:17:30 -0300

    Mudancas nas telas de assistencia

--- app/src/main/java/com/example/pedroluis/AssistenciaMenuActivity.java
+++ app/src/main/java/com/example/pedroluis/AssistenciaMenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.pedroluis;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AssistenciaMenuActivity extends AppCompatActivity {
@@ -31,10 +33,22 @@ public class AssistenciaMenuActivity extends AppCompatActivity {
         Button voltar = findViewById(R.id.voltar);
 
         fale_conosco.setOnClickListener(view ->{
-            String url = "https://api.whatsapp.com/send?phone="+number;
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(url));
-            startActivity(i);
+            AlertDialog.Builder comfirmaAssistencia = new AlertDialog.Builder(AssistenciaMenuActivity.this);
+            comfirmaAssistencia.setTitle("Desenvolvedor: Raul Moreno Pereira");
+            comfirmaAssistencia.setMessage("Estudante do Instituto Nacional de Telecomunicações (Inatel), do curso de Engenharia de Computação, atualmente" +
+                    "trabalha no desenvolvimento do aplicado AquaFarm, destinado a produtores rurais para otimizar suas produções." +
+                    "O projeto está sendo desenvolvido dentro do CS&I Lab");
+            comfirmaAssistencia.setCancelable(false);
+            comfirmaAssistencia.setPositiveButton("Pedir assistência", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int witch) {
+                    String url = "https://api.whatsapp.com/send?phone="+number;
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+            });
+            comfirmaAssistencia.setPositiveButton("Cancelar",null);
         });
 
         fale_conosco_b.setOnClickListener(view ->{
