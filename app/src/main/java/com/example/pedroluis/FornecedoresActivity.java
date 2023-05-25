39c9cfee6201273248c5c38894d6fdd01b447207
raulmp20 <raulmp02@hotmail.com>
2023-05-25 09:59:14 -0300

    Telas de mapa

--- app/src/main/java/com/example/pedroluis/FornecedoresActivity.java
+++ app/src/main/java/com/example/pedroluis/FornecedoresActivity.java
@@ -21,12 +21,23 @@ public class FornecedoresActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
     }
-
+    // LONGITUDE E LATITUDE
+    String lat;
+    String longi;
+    String nomeponto;
+    String nomeendereco;
+    String lat1;
+    String longi1;
+    String nomeponto1;
+    String nomeendereco1;
+    String lat2;
+    String longi2;
+    String nomeponto2;
+    String nomeendereco2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_find_products);
 
 
@@ -34,23 +45,132 @@ public class FornecedoresActivity extends AppCompatActivity {
         buttonSubstrato = findViewById(R.id.Substrato);
         buttonNutrientes = findViewById(R.id.Nutrientes);
         buttonPlantas = findViewById(R.id.Plantas);
-        buttonSementes = findViewById(R.id.Sementes);
+
 
         buttonSubstrato.setOnClickListener(v->{
             Toast.makeText(getApplicationContext(), "Mapa com os locais mais proximos de venda de substrato", Toast.LENGTH_SHORT).show();
+            Intent Enviadados = new Intent(FornecedoresActivity.this, MapaActivity.class);
+
+            //DEFININDO AS LAT E LONGI DOS PONTOS DE VENDAS
+            lat = "-22.25130700903474";
+            longi = "-45.70542177792108";
+
+            lat1 = "-22.251758478350066";
+            longi1 = "-45.70236248816955";
+
+            lat2 = "-22.2525582883281";
+            longi2 = "-45.84273296562191";
+
+            nomeponto = "Flora Quintal e Jardim";
+            nomeponto1 = "Shiva Plantas";
+            nomeponto2 = "Loja de Bonsai";
+
+            nomeendereco ="R. Francisco M. da Costa, 142 - Centro, Santa Rita do Sapucaí - MG, 37540-000" ;
+            nomeendereco1 ="R. Doutor José Ribeiro de Carvalho, 66 - Centro, Santa Rita do Sapucaí - MG, 37540-000";
+            nomeendereco2 = "Santa Rita do Sapucaí - MG, 37540-000";
+
+            //ENVIANDO OS DADOS DE LAT E LONGI COM O NOME DO ESTABELECIMENTO
+            Enviadados.putExtra("Latitude", lat);
+            Enviadados.putExtra("Longitude", longi);
+            Enviadados.putExtra("ponto", nomeponto);
+            Enviadados.putExtra("endereco", nomeendereco);
+
+            Enviadados.putExtra("Latitude1", lat1);
+            Enviadados.putExtra("Longitude1", longi1);
+            Enviadados.putExtra("ponto1", nomeponto1);
+            Enviadados.putExtra("endereco1", nomeendereco1);
+
+
+            Enviadados.putExtra("Latitude2", lat2);
+            Enviadados.putExtra("Longitude2", longi2);
+            Enviadados.putExtra("ponto2", nomeponto2);
+            Enviadados.putExtra("endereco2", nomeendereco2);
+            startActivity(Enviadados);
         });
 
         buttonNutrientes.setOnClickListener(v->{
             Toast.makeText(getApplicationContext(), "Mapa com os locais mais proximos de venda de nutrientes", Toast.LENGTH_SHORT).show();
+            Intent Enviadados = new Intent(FornecedoresActivity.this, MapaActivity.class);
+
+            //DEFININDO AS LAT E LONGI DOS PONTOS DE VENDAS
+            lat = "-22.25409846381255";
+            longi = "-45.70356814678672";
+
+            lat1 = "-22.24507838556926";
+            longi1 = "-45.71000042430278";
+
+            lat2 = "-22.432416753096856";
+            longi2 = "-45.46694495360506";
+
+            nomeponto = "Casa do Farelo";
+            nomeponto1 = "Bonagro Soluções Agrícolas";
+            nomeponto2 = "P j Fertilizante";
+
+            nomeendereco ="R. Adélino Carneiro Pinto, 22 - Centro, Santa Rita do Sapucaí - MG, 37540-000" ;
+            nomeendereco1 ="R. José Pinto Vilela, n° 645 - Ozório Machado, Santa Rita do Sapucaí - MG, 37540-000";
+            nomeendereco2 = "R. Geraldino Campista, 342 - Santo Antonio, Itajubá - MG, 37503-130";
+
+            //ENVIANDO OS DADOS DE LAT E LONGI COM O NOME DO ESTABELECIMENTO
+            Enviadados.putExtra("Latitude", lat);
+            Enviadados.putExtra("Longitude", longi);
+            Enviadados.putExtra("ponto", nomeponto);
+            Enviadados.putExtra("endereco", nomeendereco);
+
+            Enviadados.putExtra("Latitude1", lat1);
+            Enviadados.putExtra("Longitude1", longi1);
+            Enviadados.putExtra("ponto1", nomeponto1);
+            Enviadados.putExtra("endereco1", nomeendereco1);
+
+
+            Enviadados.putExtra("Latitude2", lat2);
+            Enviadados.putExtra("Longitude2", longi2);
+            Enviadados.putExtra("ponto2", nomeponto2);
+            Enviadados.putExtra("endereco2", nomeendereco2);
+            startActivity(Enviadados);
         });
 
         buttonPlantas.setOnClickListener(v->{
             Toast.makeText(getApplicationContext(), "Mapa com os locais mais proximos de venda de mudas", Toast.LENGTH_SHORT).show();
+            Intent Enviadados = new Intent(FornecedoresActivity.this, MapaActivity.class);
+
+            //DEFININDO AS LAT E LONGI DOS PONTOS DE VENDAS
+            lat = "-22.244142537373907";
+            longi = "-45.70785550096605";
+
+            lat1 = "-22.25538100269502";
+            longi1 = "-45.697547257791875";
+
+            lat2 = "-22.253945030490787";
+            longi2 = "-45.70355164254022";
+
+            nomeponto = "Casa das Rações";
+            nomeponto1 = "Agropecuária Santa Luzia";
+            nomeponto2 = "Casa do Farelo";
+
+            nomeendereco ="R. Padre Vítor - Maristela, Santa Rita do Sapucaí - MG, 37540-000" ;
+            nomeendereco1 ="R. Felíciano Teles, 264 - Inatel, Santa Rita do Sapucaí - MG, 37540-000";
+            nomeendereco2 = "R. Adélino Carneiro Pinto, 22 - Centro, Santa Rita do Sapucaí - MG, 37540-000";
+
+            //ENVIANDO OS DADOS DE LAT E LONGI COM O NOME DO ESTABELECIMENTO
+            Enviadados.putExtra("Latitude", lat);
+            Enviadados.putExtra("Longitude", longi);
+            Enviadados.putExtra("ponto", nomeponto);
+            Enviadados.putExtra("endereco", nomeendereco);
+
+            Enviadados.putExtra("Latitude1", lat1);
+            Enviadados.putExtra("Longitude1", longi1);
+            Enviadados.putExtra("ponto1", nomeponto1);
+            Enviadados.putExtra("endereco1", nomeendereco1);
+
+
+            Enviadados.putExtra("Latitude2", lat2);
+            Enviadados.putExtra("Longitude2", longi2);
+            Enviadados.putExtra("ponto2", nomeponto2);
+            Enviadados.putExtra("endereco2", nomeendereco2);
+            startActivity(Enviadados);
         });
 
-        buttonSementes.setOnClickListener(v->{
-            Toast.makeText(getApplicationContext(), "Mapa com os locais mais proximos de venda de sementes", Toast.LENGTH_SHORT).show();
-        });
+
 
         voltar.setOnClickListener(view ->{
             Intent intent = new Intent(FornecedoresActivity.this, MenuUsuarioActivity.class);
--- app/src/main/java/com/example/pedroluis/MapaActivity.java
+++ app/src/main/java/com/example/pedroluis/MapaActivity.java
@@ -1,38 +1,131 @@
 package com.example.pedroluis;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapaActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapaActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
 
+    float zoom = 11.0f; //VARIÁVEL PARA O ZOOM VAI ATÉ 21.0F
+
+    String lat;
+    String longi;
+    String nomeponto;
+    String nomeendereco;
+    String lat1;
+    String longi1;
+    String nomeponto1;
+    String nomeendereco1;
+    String lat2;
+    String longi2;
+    String nomeponto2;
+    String nomeendereco2;
+
+    double lo;
+    double la;
+    double lo1;
+    double la1;
+    double lo2;
+    double la2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_mapa);
+
+        Button voltar = findViewById(R.id.button_voltar_mapa);
+        voltar.setOnClickListener(v->{
+            Intent intent = new Intent(MapaActivity.this, FornecedoresActivity.class);
+            startActivity(intent);
+        });
+
+
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Adiciona o marcador em santa rita
+        LatLng SRS = new LatLng(-22.252980039557748, -45.704704303946194);
+
+        //Gerando os marcadores nos pontos de venda
+        lat = getIntent().getStringExtra("Latitude");
+        longi = getIntent().getStringExtra("Longitude");
+
+        lat1 = getIntent().getStringExtra("Latitude1");
+        longi1 = getIntent().getStringExtra("Longitude1");
+
+        lat2 = getIntent().getStringExtra("Latitude2");
+        longi2 = getIntent().getStringExtra("Longitude2");
+
+        nomeponto = getIntent().getStringExtra("ponto");
+        nomeponto1 = getIntent().getStringExtra("ponto1");
+        nomeponto2 = getIntent().getStringExtra("ponto2");
+
+        nomeendereco = getIntent().getStringExtra("endereco");
+        nomeendereco1 = getIntent().getStringExtra("endereco1");
+        nomeendereco2 = getIntent().getStringExtra("endereco2");
+        la = Double.parseDouble(lat);
+        lo = Double.parseDouble(longi);
+
+        la1 = Double.parseDouble(lat1);
+        lo1 = Double.parseDouble(longi1);
+
+        la2 = Double.parseDouble(lat2);
+        lo2 = Double.parseDouble(longi2);
+
+        LatLng ponto = new LatLng(la, lo);
+        LatLng ponto1 = new LatLng(la1, lo1);
+        LatLng ponto2 = new LatLng(la2, lo2);
+
+        mMap.addMarker(new MarkerOptions().position(ponto).title(nomeponto));
+        mMap.addMarker(new MarkerOptions().position(ponto1).title(nomeponto1));
+        mMap.addMarker(new MarkerOptions().position(ponto2).title(nomeponto2));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SRS,zoom));
+        final LatLng EnderecoLatLng = new LatLng(la, lo);
+        Marker Ponto = mMap.addMarker(
+                new MarkerOptions()
+                        .position(EnderecoLatLng)
+                        .title(nomeendereco));
+        Ponto.showInfoWindow();
+
+        final LatLng Endereco1LatLng = new LatLng(la1, lo1);
+        Marker Ponto1 = mMap.addMarker(
+                new MarkerOptions()
+                        .position(Endereco1LatLng)
+                        .title(nomeendereco1));
+        Ponto1.showInfoWindow();
+
+        final LatLng Endereco2LatLng = new LatLng(la2, lo2);
+        Marker Ponto2 = mMap.addMarker(
+                new MarkerOptions()
+                        .position(Endereco2LatLng)
+                        .title(nomeendereco2));
+        Ponto2.showInfoWindow();
     }
+
+
 }
